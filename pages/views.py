ddc203c08536a082854da04e3141b9025e1a5b69
Freddy Barrera Artiaga <fredy.barrera.a@gmail.com>
2020-04-13 12:47:50 -0400

    Mixin de indentificación

--- pages/views.py
+++ pages/views.py
@@ -2,22 +2,34 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+# Este mixin requiere que el usuario sea #niembro del staff
+class StaffRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 
 # Create your views here.
 class PageListView(ListView):
     model = Page
 
+
 class PageDetailView(DetailView):
     model = Page
     
-class PageCreate(CreateView):
+
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
-class PageUpdate(UpdateView):
+
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -25,6 +37,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDelete(DeleteView):
+
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
